da9804e1bff688169cfc7479825be21bf9e44e0d
Ruben Laguna <ruben.laguna@tele2.com>
2024-07-24 10:36:46 +0200

    runFile and runPrompt

--- chapter04/app/src/main/java/com/craftinginterpreters/lox/Lox.java
+++ chapter04/app/src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -1,23 +1,40 @@
 package com.craftinginterpreters.lox;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 public class Lox {
-  public static void main(String[] args) throws IOException {
-    if (args.length > 1) {
-      System.out.println("Usage: jlox [script]");
-      System.exit(64);
-    } else if (args.length == 1) {
-      System.out.println("runFile");
-      // runFile(args[0]);
-    } else {
-      System.out.println("runPrompt");
-      // runPrompt();
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            System.out.println("runFile");
+            // runFile(args[0]);
+        } else {
+            System.out.println("runPrompt");
+            // runPrompt();
+        }
     }
+
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.getDefaultCharset()));
+  }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+    for (;;) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null) break;
+      run(line)
+    }
+  }
+
+
 }
-}
+
+
